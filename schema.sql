3641be8355e0ff985417be5e4f23bcad7bc6275d
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-08-11 15:45:39 -0500

    Create new tables and linked them as foreign keys

--- schema.sql
+++ schema.sql
@@ -10,3 +10,27 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species VARCHAR(50);
+
+-- DAY 3
+-- Delete species column from animals table, create two new tables, one for species and one for owners, then add new columns to animals table called species_id and owners_id and add a foreign key constraint to animals table.
+
+CREATE TABLE owners(
+    id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    full_name VARCHAR(50)
+    age INT
+);
+
+CREATE TABLE species(
+    id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    name VARCHAR(50)
+);
+
+ALTER TABLE animals 
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owners_id INT REFERENCES owners(id);
+
