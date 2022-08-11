5272bce61d68d0274f7f7a7c89221424b4a34e62
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-08-10 19:52:15 -0500

    Revert to initial commit to fix gitflow

--- schema.sql
+++ schema.sql
@@ -1,10 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-    name VARCHAR(50),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL
+    name varchar(100)
 );
