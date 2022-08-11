d7b5c468ddc9fbb7761dc9c41b4a4f3d662e82d6
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-08-10 23:18:42 -0500

    Create transaction updates

--- data.sql
+++ data.sql
@@ -8,15 +8,13 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devilmon', '2017-05-12', 5, 'yes', 11);
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Charmander', '2020-02-08', 0, 'no', 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Charmander', '2020-02-08', 0, 'no', -11);
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Plantmon', '2021-11-15', 2, 'yes', 5.7);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Plantmon', '2021-11-15', 2, 'yes', -5.7);
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', '1993-04-02', 3, 'no', 12.13);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', '1993-04-02', 3, 'no', -12.13);
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Angemon', '2005-06-12', 1, 'yes' 45);
-                                                 ^
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Angemon', '2005-06-12', 1, 'yes', 45);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Angemon', '2005-06-12', 1, 'yes' -45);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Boarmon', '2005-06-07', 7, 'yes', 20.4);
 
@@ -43,3 +41,15 @@ DELETE FROM animals;
 SELECT * FROM animals;
 
 ROLLBACK;
+
+-- REMOVE ALL ANIMALS BORN AFTER 2022-01-01, CREATE SAVEPOINT AND UPDATE ALL ANIMALS WEIGHT TO THEIR WEIGHT MULTIPLIED BY -1
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT no2022;
+UPDATE animals SET weight_kg = weight_kg * -1;
+-- ROLLBACK TO SAVEPOINT AND UPDATE ALL ANIMALS' NEGATIVE WEIGHT TO THEIR WEIGHT MULTIPLIED BY -1
+ROLLBACK TO SAVEPOINT no2022;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
